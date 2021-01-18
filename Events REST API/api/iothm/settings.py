b0dc3968385908dfc29ac7ca46e8821f6f4a7869
Fernando França (フランサ) <github@furansa.me>
2021-01-18 20:48:27 +0000

    Added pagination

--- Events REST API/api/iothm/settings.py
+++ Events REST API/api/iothm/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "iothm.wsgi.application"
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
